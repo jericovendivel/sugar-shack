4ee1a5e1df308bc636be62a7b34d2d5447ffb492
jericovendivel <jericovendivel122@gmail.com>
2025-11-29 22:42:58 +0800

    Add header.php for Sugar Shack Ordering System

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Sugar Shack Ordering System</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+<nav>
+    <a href="VariablesAndArrays.php">Store Info</a> |
+    <a href="ExpressionsAndOperators.php">Calculator</a> |
+    <a href="shorthandEcho.php">Order Summary</a> |
+    <a href="typejuggling.php">Type Juggling</a>
+</nav>
+
+<hr>
