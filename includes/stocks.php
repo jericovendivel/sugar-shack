490094fb53c1cb165367e71b4bcf50db619e63d0
jericovendivel <jericovendivel122@gmail.com>
2025-12-03 14:37:28 +0800

    Add files via upload

---
+++ includes/stocks.php
@@ -0,0 +1,11 @@
+<?php
+// Centralized product inventory
+
+$sugon_products = [
+    "Crinkle Cookies"     => ["price" => 25.00, "stock" => 18],
+    "NY Cookies"          => ["price" => 35.00, "stock" => 7],
+    "Milk Bars"           => ["price" => 40.00, "stock" => 22],
+    "Chocolate Loaf"      => ["price" => 120.00, "stock" => 5],
+    "Ube Cheesecake Cup"  => ["price" => 55.00, "stock" => 10],
+    "Biscoff Bar"         => ["price" => 50.00, "stock" => 3]
+];
