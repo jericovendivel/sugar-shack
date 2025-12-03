1a3892fff52e70e1b6c7679694963006cdf91d9a
jericovendivel <jericovendivel122@gmail.com>
2025-12-03 14:36:43 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Sugar Shack Ordering System</title>
+    <title>Sugar Shack</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
@@ -11,7 +11,8 @@
     <a href="VariablesAndArrays.php">Store Info</a> |
     <a href="ExpressionsAndOperators.php">Calculator</a> |
     <a href="shorthandEcho.php">Order Summary</a> |
-    <a href="typejuggling.php">Type Juggling</a>
+    <a href="typejuggling.php">Type Juggling</a> |
+    <a href="StockMonitoring.php">Stock Monitoring</a>
 </nav>
 
 <hr>
